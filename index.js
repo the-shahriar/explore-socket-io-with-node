d85a8fa8ed24fc653caae02cff99544236d2857b
Shahriar Islam <theshahriarislam@gmail.com>
2021-11-23 18:51:46 +0600

    added css to ui

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const express = require('express');
 const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
 
 const port = process.env.PORT || 5000;
 
+app.use(express.static(__dirname + '/public'))
+
+
+http.listen(port, ()=> {
+    console.log('Server is running');
+})
+
+app.get('/', (req, res)=> {
+    res.sendFile(__dirname + '/index.html');
+})
+
+
+// socket
+io.on('connection', (socket)=> {
+    socket.on('message', (msg)=> {
+        socket.broadcast.emit('message', msg);
+    })
+})
---
+++ public/client.js
@@ -0,0 +1,47 @@
+const socket = io();
+
+let name;
+let textarea = document.querySelector('#textarea');
+let messageArea = document.querySelector('.message__area');
+
+do {
+    name = prompt('Please enter your name');
+} while(!name)
+
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter'){
+        sendMessage(e.target.value);
+    }
+})
+
+const sendMessage = (message) => {
+    let msg = {
+        user: name,
+        message: message.trim()
+    }
+    // appendMessage
+    appendMessage(msg, 'outgoing');
+
+    // send to server
+    socket.emit('message', msg)
+}
+
+// append
+const appendMessage = (msg, type) => {
+    let mainDiv = document.createElement('div');
+    let className = type;
+    mainDiv.classList.add(className, 'message');
+    let markup = `
+        <h4>${msg.user}</h4>
+        <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML = markup;
+
+    messageArea.appendChild(mainDiv);
+
+}
+
+// receive message from server
+socket.on('message', (msg)=> {
+    appendMessage(msg, 'incoming');
+})
